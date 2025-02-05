01c4299747f9ef4e7c6347f8c9406b51cb557800
SATYAJIT ROY <satyajitroy19599@gmsil.com>
2025-02-05 16:01:26 +0530

    Add JS file

---
+++ mainWeb/allCodes/script.js
@@ -0,0 +1,15 @@
+VanillaTilt.init(document.querySelector(".member1"), {
+    max: 25,
+    speed: 400,
+    glare:true
+});
+VanillaTilt.init(document.querySelector(".member2"), {
+    max: 25,
+    speed: 400,
+    glare:true
+});
+VanillaTilt.init(document.querySelector(".member3"), {
+    max: 25,
+    speed: 400,
+    glare:true
+});
