5763f482c760f847ca1434534d079b16d6859b70
SATYAJIT ROY <satyajitroy19599@gmsil.com>
2025-02-13 15:28:20 +0530

    Add 5th page

---
+++ mainWeb/allCodes/script_1js.js
@@ -0,0 +1,5 @@
+let conf=document.querySelector(".main_container .fiveth_page .fiveth_1st .input_box .content button");
+conf.onclick=(e)=>{
+alert('Sent in your email ! \n Thanks for be a part of us 🧡')
+
+};
